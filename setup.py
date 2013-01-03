e7fedfb028ce51f737de9f935ab86ab4a64ff98e
James Addison <code@scottisheyes.com>
2013-01-03 11:15:58 -0800

    Updating setup.py with new version and simpler description (pointing to Github for documentation).

--- setup.py
+++ setup.py
@@ -1,19 +1,14 @@
-import os
 from distutils.core import setup
-
-
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
 setup(
     name = 'django-simple-templates',
     description = 'Easy, designer-friendly templates and A/B testing friendly tools for Django.',
-    long_description=read('README.md'),
+    long_description=u'View `django-simple-templates documentation on Github  <https://github.com/jaddison/django-simple-templates>`_.',
     author='James Addison',
     author_email='code@scottisheyes.com',
     packages = ['simple_templates'],
-    version = '0.5.1',
+    version = '0.6.0',
     url='http://github.com/jaddison/django-simple-templates',
     keywords=['a/b testing', 'split testing', 'a/b', 'split'],
     license='BSD',
