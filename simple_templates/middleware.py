e529ca42691a61b7759935f0fdb7a5ab2f93de5e
James Addison <code@scottisheyes.com>
2013-01-03 11:08:19 -0800

    Switching string formatting to use unicode strings.

--- simple_templates/middleware.py
+++ simple_templates/middleware.py
@@ -14,18 +14,18 @@ class SimplePageFallbackMiddleware(object):
             return response
 
         # set up the location where this template should reside
-        template = "{0}/{1}.html".format(SIMPLE_TEMPLATES_DIR, request.path.strip('/') or '_homepage_')
+        template = u"{0}/{1}.html".format(SIMPLE_TEMPLATES_DIR, request.path.strip('/') or u'_homepage_')
 
         # if it doesn't exist, continue with the 404 response
         if not find_template(template):
             return response
 
         # if the template exists, ensure the trailing slash is present and redirect if necessary
-        if not request.path.endswith('/') and settings.APPEND_SLASH:
-            url = request.path + "/"
+        if not request.path.endswith(u'/') and settings.APPEND_SLASH:
+            url = request.path + u"/"
             qs = request.META.get('QUERY_STRING')
             if qs:
-                url += '?' + qs
+                url += u'?' + qs
             return redirect(url, permanent=True)
 
         # check for the presence of an a/b test template page
