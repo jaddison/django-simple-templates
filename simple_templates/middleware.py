76adfe5c61fa70541f6f0d8972142d5d7c3a13eb
James Addison <jaddison@Jamess-MacBook-Pro.local>
2025-02-11 14:26:43 -0800

    Ensure middleware is async-able.

--- simple_templates/middleware.py
+++ simple_templates/middleware.py
@@ -1,3 +1,5 @@
+import asyncio
+
 from django.conf import settings
 from django.views.decorators.csrf import requires_csrf_token
 from django.shortcuts import render, redirect
@@ -10,10 +12,22 @@ SIMPLE_TEMPLATES_DIR = getattr(settings, 'SIMPLE_TEMPLATES_DIR', 'simple_templat
 class SimplePageFallbackMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
+        self.is_async = asyncio.iscoroutinefunction(self.get_response)
 
     def __call__(self, request):
+        if self.is_async:
+            return self.__acall__(request)
+
         response = self.get_response(request)
 
+        return self.process_response(request, response)
+
+    async def __acall__(self, request):
+        response = await self.get_response(request)
+
+        return self.process_response(request, response)
+
+    def process_response(self, request, response):
         # No need to check for a simple template for non-404 responses.
         if response.status_code != 404:
             return response
